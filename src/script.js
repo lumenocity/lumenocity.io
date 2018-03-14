d0e15ea82782a4e15d17c182059f2ae8fbadecaa
mtimofiiv <github@fiiv.io>
2018-03-14 11:37:58 +0100

    updated links

--- src/script.js
+++ src/script.js
@@ -1,12 +1,18 @@
-function scrollToWhy() {
-  document.getElementById('why-lumenocity').scrollIntoView({
-    behavior: 'smooth'
-  });
+function scrollTo(selector) {
+  return function () {
+    document.getElementById(selector).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
 }
 
 function init() {
   for (const el of document.querySelectorAll('.scroll-to-why')) {
-    el.addEventListener('click', scrollToWhy);
+    el.addEventListener('click', scrollTo('why-wallet-content'));
+  }
+
+  for (const el of document.querySelectorAll('.scroll-to-get')) {
+    el.addEventListener('click', scrollTo('get-app'));
   }
 }
 
